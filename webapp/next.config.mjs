562127dc261b55f462281c2b457a134ac7c8c370
sukanta <sukantadandapat1998@gmail.com>
2024-08-18 23:01:57 +0530

    initial setup

--- webapp/next.config.mjs
+++ webapp/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+      },
+      reactStrictMode: false,
+      experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
+};
 
 export default nextConfig;
--- webapp/src/app/(auth)/login/page.tsx
+++ webapp/src/app/(auth)/login/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 const name : string = 'Next App'
+const names : string = 'Next App'
 
 export default function page() {
   return (
